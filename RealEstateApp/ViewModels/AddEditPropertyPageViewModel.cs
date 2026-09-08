ca8045f9d5461ac5e577128c2bc2c4a023c18b43
Jonasthepanda67 <jonas.petersen@danfoss.com>
2026-09-08 11:13:14 +0200

    Added implementation for battery status, to give the user a notice based on their battery level.

--- RealEstateApp/ViewModels/AddEditPropertyPageViewModel.cs
+++ RealEstateApp/ViewModels/AddEditPropertyPageViewModel.cs
@@ -9,7 +9,6 @@ namespace RealEstateApp.ViewModels;
 [QueryProperty(nameof(Property), "MyProperty")]
 public class AddEditPropertyPageViewModel : BaseViewModel
 {
-    public bool IsGeocodeAddressButtonVisible { get; set; }
     private readonly IPropertyService service;
 
     public AddEditPropertyPageViewModel(IPropertyService service)
@@ -17,13 +16,17 @@ public class AddEditPropertyPageViewModel : BaseViewModel
         this.service = service;
         Agents = new ObservableCollection<Agent>(service.GetAgents());
 
+
+        Battery.BatteryInfoChanged += OnBatteryInfoChanged;
+        Battery.EnergySaverStatusChanged += OnEnergySaverStatusChanged;
         Connectivity.ConnectivityChanged += OnConnectivityChanged;
         _ = CheckConnection();
+        _ = BatteryStatusCheck();
     }
 
     public string Mode { get; set; }
 
-    #region PROPERTIES
+    #region Properties
     public ObservableCollection<Agent> Agents { get; }
 
     private Property _property;
@@ -110,6 +113,7 @@ public class AddEditPropertyPageViewModel : BaseViewModel
 
     #region Location
 
+    public bool IsGeocodeAddressButtonVisible { get; set; }
     private Command getCurrentLocationCommand;
     public ICommand GetCurrentLocationCommand =>
         getCurrentLocationCommand ??= new Command(
@@ -310,6 +314,39 @@ public class AddEditPropertyPageViewModel : BaseViewModel
         }
     }
 
+
+    #endregion
+
+    #region Battery
+
+    private async void OnBatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e) => await BatteryStatusCheck();
+    private async void OnEnergySaverStatusChanged(object sender, EnergySaverStatusChangedEventArgs e) => await BatteryStatusCheck();
+    private async Task BatteryStatusCheck()
+    {
+        if (Battery.ChargeLevel < 0.2)
+        {
+            StatusColor = Colors.Red;
+            StatusMessage = "Battery is low. Please charge your device.";
+        }
+        else if (Battery.State == BatteryState.Charging)
+        {
+            StatusColor = Colors.Orange;
+            StatusMessage = "Battery is charging.";
+        }
+        else
+        {
+            StatusColor = Colors.Green;
+            StatusMessage = "";
+        }
+
+            if (Battery.EnergySaverStatus == EnergySaverStatus.On)
+        {
+            StatusColor = Colors.Green;
+            StatusMessage = "Energy saver is on. Some features may be limited.";
+        }
+        else
+            StatusMessage = "";
+    }
 
     #endregion
 
