78e838208c8739ba9eafe0a51745148544e96eff
Jonasthepanda67 <jonas.petersen@danfoss.com>
2026-09-09 09:23:23 +0200

    Added vendors as a new model and added some to the properties. Added email, call and sms function.

--- RealEstateApp/Models/Property.cs
+++ RealEstateApp/Models/Property.cs
@@ -14,17 +14,20 @@
         public string Address { get; set; }
         public int? Price { get; set; }
         public string Description { get; set; }
+        public string VendorId { get; set; }
+        public Vendor? Vendor { get; set; }
         public PropertyType Type { get; set; }
         public PropertyTier Tier { get; set; }
         public int? Beds { get; set; }
         public int? Baths { get; set; }
         public int? Parking { get; set; }
         public int? LandSize { get; set; }
+        public string NeighbourhoodUrl { get; set; } = string.Empty;
         public string AgentId { get; set; }
-        public List<string> ImageUrls { get; set; }
         public double? Latitude { get; set; }
         public double? Longitude { get; set; }
-        public string Aspect { get; set; }
+        public string Aspect { get; set; } = string.Empty;
+        public List<string> ImageUrls { get; set; }
 
         public string MainImageUrl => ImageUrls?.FirstOrDefault() ?? GlobalSettings.Instance.NoImageUrl;
     }
